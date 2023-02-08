f1f1aaafa179a1908c1a57dd5f99c664be711e77
gh_masoud <msd.drg@gmail.com>
2023-02-08 15:17:00 +0330

    [ADD] init react project with typescript + tailwind + jsonServer + some other package

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="text-red-800">App</div>;
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import "./index.css";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+
+const el = document.getElementById("root") as HTMLElement;
+const root = createRoot(el);
+
+root.render(<App />);
